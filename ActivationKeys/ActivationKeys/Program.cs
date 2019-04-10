950450bb50c8872d46b32ab88b1f89f9293d8255
Petko <limona.pb@gmail.com>
2019-04-10 11:57:43 +0300

    Practice Dictionaries and Text Processing

---
+++ ActivationKeys/ActivationKeys/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ActivationKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wrongKeys = Console.ReadLine().Split('&');
+            List<string> validKeys = new List<string>();
+            for (int i = 0; i < wrongKeys.Length; i++)
+            {
+                bool isNotLetterOrDig = true;
+                for (int symbol = 0; symbol < wrongKeys[i].Length; symbol++)
+                {
+                    if (!char.IsLetter(wrongKeys[i][symbol]) && !char.IsDigit(wrongKeys[i][symbol]))
+                    {
+                        isNotLetterOrDig = false;
+                    }
+                }
+                bool isLong = false;
+                if (isNotLetterOrDig)
+                {
+                    if (wrongKeys[i].Length == 16 || wrongKeys[i].Length == 25)
+                    {
+                        isLong = true;
+                    }
+                }
+                if (isNotLetterOrDig == true && isLong == true)
+                {
+                    StringBuilder key = new StringBuilder();
+                    if (wrongKeys[i].Length == 16)
+                    {
+                        for (int j = 0; j < 16; j++)
+                        {
+                            char letter = wrongKeys[i][j];
+                            if (char.IsLetter(letter) && char.IsLower(letter))
+                            {
+                                letter = char.ToUpper(letter);
+                                key.Append(letter);
+                                continue;
+                            }
+                            if (char.IsDigit(letter))
+                            {
+                                int digit = Math.Abs((int)char.GetNumericValue(letter) - 9);
+                                key.Append(digit);
+                                continue;
+                            }
+                            key.Append(letter);
+                        }
+                    }
+                    if (wrongKeys[i].Length == 25)
+                    {
+                        for (int j = 0; j < wrongKeys[i].Length; j++)
+                        {
+                            char letter = wrongKeys[i][j];
+                            if (char.IsLetter(letter) && char.IsLower(letter))
+                            {
+                                letter = char.ToUpper(letter);
+                                key.Append(letter);
+                                continue;
+                            }
+                            if (char.IsDigit(letter))
+                            {
+                                int digit = Math.Abs((int)char.GetNumericValue(letter) - 9);
+                                key.Append(digit);
+                                continue;
+                            }
+                            key.Append(letter);
+                        }
+                    }
+                    int lenght = key.Length;
+                    int groubsBy = 0;
+                    if (key.Length == 16)
+                    {
+                        groubsBy = 4;
+                    }
+                    if (key.Length == 25)
+                    {
+                        groubsBy = 5;
+                    }
+                    StringBuilder validKey = new StringBuilder();
+                    int count = 0;
+                    for (int j = 0; j < lenght; j++)
+                    {
+                        char letter = key[j];
+                        if (count == groubsBy)
+                        {
+                            validKey.Append('-');
+                            count = 0;
+                        }
+                        count++;
+                        validKey.Append(letter);
+                    }
+                    validKeys.Add(validKey.ToString());
+                }
+            }
+            Console.WriteLine(string.Join(", ",validKeys));
+        }
+    }
+}
---
+++ VaporWinterSale/VaporWinterSale/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VaporWinterSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gamesAndDlcs = Console.ReadLine().Split(", ");
+            char[] splitters = new char[2];
+            splitters[0]= '-';
+            splitters[1] = ':';
+            Dictionary<string, decimal> games = new Dictionary<string, decimal>();
+            Dictionary<string, decimal> gamesWithDlcs = new Dictionary<string, decimal>();
+            for (int i = 0; i < gamesAndDlcs.Length; i++)
+            {
+                string[] gamePriceOrDlc = gamesAndDlcs[i].Split(splitters);
+                string game = gamePriceOrDlc[0];
+                decimal price = 0M;
+                if (char.IsDigit(gamePriceOrDlc[1][0]) && char.IsDigit(gamePriceOrDlc[1][gamePriceOrDlc[1].Length-1]))
+                {
+                    price = decimal.Parse(gamePriceOrDlc[1]);
+                }
+                if (price != 0)
+                {
+                    if (!games.ContainsKey(game))
+                    {
+                        games.Add(game, price);
+                    }
+                }
+                else
+                {
+                    string dlc = gamePriceOrDlc[1];
+                    if (games.ContainsKey(game))
+                    {
+                        decimal gamePrice = games[game];
+                        games.Remove(game);
+                        decimal offFromPrice = gamePrice * 0.20M;
+                        gamePrice += offFromPrice;
+                        string gameWithDlc = $"{game} - {dlc}";
+                        gamesWithDlcs.Add(gameWithDlc, gamePrice);
+                    }
+                }
+            }
+            foreach (var game in games.ToList())
+            {
+                decimal gamePriceOff = game.Value * 0.20M;
+                games[game.Key] -= gamePriceOff;
+            }
+            foreach (var game in gamesWithDlcs.ToList())
+            {
+                decimal offPrice = game.Value * 0.50M;
+                gamesWithDlcs[game.Key] -= offPrice;
+            }
+            gamesWithDlcs = gamesWithDlcs.OrderBy(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var game in gamesWithDlcs)
+            {
+                Console.WriteLine($"{game.Key} - {game.Value:f2}");
+            }
+
+            games = games.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var game in games)
+            {
+                Console.WriteLine($"{game.Key} - {game.Value:f2}");
+            }
+        }
+    }
+}
